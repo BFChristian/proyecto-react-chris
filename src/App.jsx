356d53c1620d4519d4c5d6fe4df71a10f5843de0
Christian <b.christian.fuentes@gmail.com>
2021-01-26 14:47:21 -0300

    clase 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/navbar/';
+function App() {
+  return (
+    <>
+    <NavbarComponent/>
+    <h2>Bienvenido al sitio</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+function NavbarComponent() {
+  return (
+    <nav>
+      <h1>WercadoLibre</h1>
+      <ul>{<p><a href="#">Remeras</a></p>}{<p><a href="#">Zapatillas</a></p>}</ul>
+      
+      
+    </nav>
+  );
+}
+
+export default NavbarComponent;
+
